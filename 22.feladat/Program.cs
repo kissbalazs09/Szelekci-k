f469ca25cba3e524d299f7eece860ee60f4e510e
kissbalazs09 <kiss.balazs.1225@dszcbaross.edu.hu>
2026-09-24 10:55:09 +0200

    23.feladat kész

---
+++ 22.feladat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+
+            if (szam % 3 == 0)
+            {
+                Console.WriteLine("A szám osztható 3-mal.");
+            }
+            else
+            {
+                Console.WriteLine("A szám nem osztható 3-mal.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 23.feladat/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23.feladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kérek egy számot: ");
+            int szam1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Kérek egy második számot: ");
+            int szam2 = int.Parse(Console.ReadLine());
+
+            if (szam1 > szam2)
+            {
+                Console.WriteLine($"Eredmény: {szam1 - szam2}");
+            }
+            else if (szam1 < szam2)
+            {
+                Console.WriteLine($"Eredmény: {szam2 - szam1}");
+            }
+            else
+            {
+                Console.WriteLine($"{szam1} = {szam2}");
+            }
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
